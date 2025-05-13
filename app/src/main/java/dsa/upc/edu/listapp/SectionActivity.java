5ef25028afe3180ba1e556630304c557c45e0c4a
junjie wang <junjie7311@gmail.com>
2025-05-13 21:41:38 +0200

    detalles_icono

--- app/src/main/java/dsa/upc/edu/listapp/SectionActivity.java
+++ app/src/main/java/dsa/upc/edu/listapp/SectionActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -56,7 +57,7 @@ public class SectionActivity extends AppCompatActivity {
         setTitle(categoria);
 
         // Botón Carrito
-        Button btnCart = findViewById(R.id.btnCart);
+        ImageButton btnCart = findViewById(R.id.btnCart);
         btnCart.setOnClickListener(v -> {
             Intent i = new Intent(SectionActivity.this, CarritoActivity.class);
             i.putExtra("idPartida", idPartida);
